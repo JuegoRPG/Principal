0d268d9917340abe79fc98e55f83e61aab3a91d5
DavidRigueira <32130206+DavidRigueira@users.noreply.github.com>
2018-07-27 15:39:18 +0200

    Update Mapa.java

--- Clases/Mapa.java
+++ Clases/Mapa.java
@@ -15,39 +15,4 @@ public class Mapa {
      */
     String zona;
     double cantidadZonas=0;
-    /**
-     * Metodo para crar zonas de forma aleatoria hasta un limite en este caso 10 zonas aleatorias
-     */
-    public void crearZona(){
-        String zona;
-        int aleatorio;
-        aleatorio = (int) (Math.random() * 5 + 1);//Deberia escoger entre 1 y 5 para crear 10 zonas distintas
-        if (cantidadZonas > 10){//Limitador de zonas a crear
-        switch(aleatorio){
-             case 1:
-                zona = "Desertico";
-                System.out.println(zona);
-                break;
-             case 2:
-                zona = "Boscosa";
-                System.out.println(zona);
-                break;
-             case 3:
-                zona = "Acuatica";
-                System.out.println(zona);
-                break;
-             case 4:
-                zona = "Montañosa";
-                System.out.println(zona);
-                break;
-             case 5:
-                zona = "Praderas";
-                System.out.println(zona);
-                break;
-        }
-        cantidadZonas++;//Contador de las zonas en aumento para salir del if
-        }else{
-            System.out.println("No se pueden crear mas zonas");
-        }
-    }
 }
